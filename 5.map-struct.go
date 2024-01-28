ac2e931139a74519c60856d078b4f1b58f56d484
Ha Yen <hanguyentieuyen@gmail.com>
2024-01-29 00:32:54 +0700

    update

--- 5.map-struct.go
+++ 5.map-struct.go
@@ -5,10 +5,17 @@ import (
 )
 
 func main() {
-	studentNameAgeMap := map[string]int{
+	studentNameAgeMap := make(map[string]int)
+	studentNameAgeMap = map[string]int{
 		"yen":  18,
 		"john": 25,
 		"mike": 35,
 	}
+	delete(studentNameAgeMap, "yen")
+	studentNameAgeMap["mike"] = 36
+	studentNameAgeMap["alice"] = 19
+	_, isExist := studentNameAgeMap["yen"]
 	fmt.Println(studentNameAgeMap)
+	fmt.Println(isExist)
+	fmt.Println(len(studentNameAgeMap))
 }
